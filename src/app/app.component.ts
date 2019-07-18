800c1da938792b140dbc7141a14a4137bdbf2075
ImsirovicAjdin <ajdika@live.com>
2019-07-18 10:50:54 +0200

    Add AppComponent.vehicles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   `
 })
 export class AppComponent  {
-  
+  vehicles = [
+    { id: 1, make: 'Toyota', color: { body: 'red', interior: 'gray' } },
+    { id: 2, make: 'Subaru', color: { body: 'red', interior: 'gray' } },
+    { id: 3, make: 'Subaru', old: "yes" },
+    { id: 4, make: 'Subaru', color: { body: 'yellow', interior: 'gray' }}
+  ]
 }
