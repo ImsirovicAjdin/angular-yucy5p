08ddcdc572e1c29a6cc7c07e5351939f94eb4128
ImsirovicAjdin <ajdika@live.com>
2019-07-18 10:53:31 +0200

    Hiding content with hidden binding

--- src/app/appchild/app-child.component.ts
+++ src/app/appchild/app-child.component.ts
@@ -3,7 +3,16 @@ import { Component, Input, OnInit } from '@angular/core'
 @Component({
   selector: 'app-child',
   template: `
-    <hr>
+        <div style="display: inline-block; overflow: auto; height: 200px; width: 140px; background: plum; color: white; margin:10px; padding: 20px;">
+            Child component
+            <div>ID: {{car?.id}}</div>
+            <div>MAKE: {{car?.make}}</div>
+            <div [hidden]="!car?.color">
+                <div>BODY COLOR: {{car?.color?.body}}</div>
+                <div>INTERIOR COLOR: {{car?.color?.interior}}</div>
+            </div>
+            <div [hidden]="!car?.old">OLD: {{car?.old}}</div>
+        </div>
   `
 })
 export class AppChildComponent {
